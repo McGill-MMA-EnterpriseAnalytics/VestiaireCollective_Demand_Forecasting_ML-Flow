5b46685674f3be4f8e16fa449866e761614acb5e
chloe <xy.he201833@gmail.com>
2025-04-27 13:08:24 -0400

    cleaned structure in .py ready to go

---
+++ project_ml_flow copy/tests/7_Load_Tests.py
@@ -0,0 +1,16 @@
+
+
+# # 7. Load Tests
+# 
+# Test if the pipeline can handle large datasets.
+
+
+
+import seller_analysis_notebook_script as sa
+import pandas as pd
+
+def test_load_big_data():
+    df_big = pd.concat([sa.df]*10, ignore_index=True)
+    assert df_big.shape[0] == 10 * sa.df.shape[0], "Data not scaled properly."
+
+print("Load test ready.")
