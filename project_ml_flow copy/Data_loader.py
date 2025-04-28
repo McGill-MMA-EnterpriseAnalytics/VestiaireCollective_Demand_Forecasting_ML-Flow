67de70589d0fe5c962b9636c5dd5080885a6ff4c
chloe <xy.he201833@gmail.com>
2025-04-28 02:41:35 -0400

    Remove cleaned_data.parquet from Git tracking

---
+++ project_ml_flow copy/Data_loader.py
@@ -0,0 +1,19 @@
+# data_loader.py
+
+import gdown
+import pandas as pd
+import os
+
+def load_data():
+    file_id = "1H3u6xOIiJIlpQvPtK4-Hm5Erc4ycUlMq"
+    url = f"https://drive.google.com/uc?id={file_id}"
+    output = "data/cleaned_data.parquet"
+    os.makedirs("data", exist_ok=True)
+
+    if not os.path.exists(output):
+        print("Downloading latest dataset...")
+        gdown.download(url, output, quiet=False)
+
+    df = pd.read_parquet(output)
+    print("Data loaded successfully!")
+    return df
